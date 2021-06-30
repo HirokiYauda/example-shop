50408ae949d98148ab9a32eb0c3897bf994d2753
HERO5151 <pixy.gtr@gmail.com>
2021-06-30 16:01:49 +0900

    ・ユーザーテーブルのファクトリー作成

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -21,8 +21,22 @@ $factory->define(User::class, function (Faker $faker) {
     return [
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
+        'zip' => null,
+        'pref_id' => null,
+        'address1' => null,
+        'address2' => null,
         'email_verified_at' => now(),
         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
         'remember_token' => Str::random(10),
+    ];
+});
+
+$factory->state(User::class, 'addAddress', function (Faker $faker) {
+    $addresies = explode('  ', $faker->address);
+    return [
+        'zip' => $addresies[0],
+        'pref_id' => $faker->numberBetween(1, 47),
+        'address1' => $addresies[1],
+        'address2' => null,
     ];
 });
